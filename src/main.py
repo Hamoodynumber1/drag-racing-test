d5ba6be1b8d223e9a3b6b70a9f2a54b6897ef4a7
Mohammad Thani <mohammad.almheiri@student.saedubai.com>
2022-06-21 12:39:43 +0400

    Test Field

--- src/main.py
+++ src/main.py
@@ -1,4 +1,18 @@
 health = 100
 print(health)
 
-print("Your health is " + str(health))
+print(str(health) + "HP")
+
+enemyHP = [10,20,30,40,50]
+
+sum = 0
+
+for i in range(0, 5):
+    sum += enemyHP[i]
+    
+def AddTwoNumbers(a, b):
+    sum = 0
+    sum = a + b
+    print(sum)
+
+AddTwoNumbers(30, 50)
