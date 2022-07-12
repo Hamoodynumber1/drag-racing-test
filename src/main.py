f58f568e7894ea2d041692fa2fe2df3c0f1e81e7
Mohammad Thani <mohammad.almheiri@student.saedubai.com>
2022-07-12 12:45:24 +0400

    Final version?
    Shortened code. Possibly finalized.

--- src/main.py
+++ src/main.py
@@ -1,18 +1,50 @@
-health = 100
-print(health)
+import random
+from turtle import Turtle, Screen
 
-print(str(health) + "HP")
+screen = Screen()
+screen.setup(1000, 700)
 
-enemyHP = [10,20,30,40,50]
+raceLine = Turtle("square", visible=False)
+raceLine.penup()
+raceLine.goto(300, 335)
+raceLine.pendown()
+raceLine.goto(300, -335)
 
-sum = 0
+turtleList = [
+    Turtle("classic", visible=False),
+    Turtle("classic", visible=False),
+    Turtle("classic", visible=False),
+    Turtle("classic", visible=False),
+]
 
-for i in range(0, 5):
-    sum += enemyHP[i]
-    
-def AddTwoNumbers(a, b):
-    sum = 0
-    sum = a + b
-    print(sum)
+turtleColorList = [
+    "blue",
+    "yellow",
+    "red",
+    "pink",
+]
 
-AddTwoNumbers(30, 50)
+for i in range(0, len(turtleList)):
+    turtleList[i].penup()
+    turtleList[i].color(turtleColorList[i])
+    turtleList[i].goto(0, i * 50 - 100)
+    turtleList[i].showturtle()
+
+while True: #infinite loop
+    for i in range(0, len(turtleList)):
+        turtleList[i].forward(random.randint(1, 10))
+
+        if turtleList[i].pos()[0] > 300:
+            screen.exitonclick()
+
+# my_turtle.color("blue")
+# my_turtle.shapesize(5, 5)
+# my_turtle.speed(1)
+# my_turtle.penup()
+# my_turtle.goto(500, 20)
+
+# my_turtle.color("yellow")
+# my_turtle.shapesize(2, 2)
+# my_turtle.speed(1)
+# my_turtle.penup()
+# my_turtle.goto(500, 20)
